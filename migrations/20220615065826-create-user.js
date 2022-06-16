363d4775295ed56e82fddbd9ba6e48df133ab04b
Andreas  Bilmar <andreasbilmar99@gmail.com>
2022-06-16 14:48:01 +0700

    change number_mobile to string

--- migrations/20220615065826-create-user.js
+++ migrations/20220615065826-create-user.js
@@ -30,7 +30,7 @@ module.exports = {
         type: Sequelize.STRING
       },
       number_mobile: {
-        type: Sequelize.INTEGER
+        type: Sequelize.STRING
       },
       createdAt: {
         allowNull: false,
