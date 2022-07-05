7bca8395d9a55c9d77d8e3f9f32e170d49bc439b
taqiudin.ahyari <taqiudin.ahyari@gmail.com>
2022-07-05 22:32:58 +0700

    api create and update transaksi

--- controller/transaksiController.js
+++ controller/transaksiController.js
@@ -3,86 +3,112 @@ module.exports = {
   createTransaksi: (req, res) => {
     let data = req.body;
 
-    models.product.findOne({
+    models.tawar.findAll({
       where: {
-        id: data.product_id,
-        publish: true,
-        isSold: false,
-      },
-    }).then((product) => {
-      if (product) {
-        models.product.update(
-          { isSold: true },
-          { where: { id: data.product_id } }
-        ).then(() => {
-          models.transaksi.create({
-            buyer_id: data.buyer_id,
-            seller_id: data.seller_id, // || req.user.id
-            productId: data.product_id,
-            price: data.price,
-          })
-            .then((transaksi) => {
-              res.status(200).json({
-                message: "Transaksi Berhasil",
-                success: true,
-                data: transaksi,
-              })
-            })
-        });
+        userId: data.userId,
+        productId: data.productId
+      }
+    }).then(result => {
+      if (result.length > 0) {
+        models.transaksi.create(data).then(result => {
+          res.status(200).json({
+            message: "Success create transaksi",
+            success: true,
+            data: result
+          });
+        }
+        ).catch(err => {
+          res.status(500).json({
+            message: err.message,
+            success: false,
+          });
+        })
       } else {
         res.status(500).json({
-          message: "Transaksi Gagal",
+          message: "Product not found in tawar",
           success: false,
         });
       }
+    }).catch(err => {
+      res.status(500).json({
+        message: err.message,
+        success: false,
+      });
     })
+
   },
 
-  getTransaksi: (req, res) => {
-
-    models.transaksi.findAll({
-      include: [
-        {
-          model: models.product, as: "product",
-        },
-      ],
-      where: {
-        seller_id: req.user.id
-      }
-    })
-      .then((transaksi) => {
-        res.status(200).json({
-          message: "Succes get transaksi",
-          success: true,
-          data: transaksi,
-        });
-      }
-      ).catch((err) => {
-        res.status(500).json({
-          message: err.message,
-          success: false,
-        });
-      })
-  },
-
-  getDetailTransaksi: (req, res) => {
+  updateTransaksi: (req, res) => {
+    let data = req.body;
     models.transaksi.findOne({
       where: {
-        id: req.params.id
+        id: data.transaksiId
       }
-    })
-      .then((result) => {
-        res.status(200).json({
-          message: "Succes get transaksi",
-          success: true,
-          data: result,
-        })
-      }).catch((err) => {
+    }).then(result => {
+      if (result != null) {
+        console.log(data);
+        if (data.status === "accepted") {
+          models.transaksi.update({
+            status: data.status
+          }, {
+            where: {
+              id: data.transaksiId
+            }
+          })
+          models.product.update({
+            isSold: true
+          }, {
+            where: {
+              id: result.productId
+            }
+          })
+          models.tawar.destroy({
+            where: {
+              productId: result.productId
+            }
+          })
+
+          res.status(200).json({
+            message: "Transaksi accepted",
+            success: true,
+          });
+        }
+
+        if (data.status === "rejected") {
+          models.transaksi.update({
+            status: data.status
+          }, {
+            where: {
+              id: data.transaksiId
+            }
+          })
+          models.tawar.destroy({
+            where: {
+              id: req.params.id
+            }
+          })
+
+          res.status(200).json({
+            message: "Transaksi rejected",
+            success: true,
+          });
+
+        }
+
+      } else {
         res.status(500).json({
-          message: err.message,
-          success: false
-        })
-      })
-  }
+          message: "Transaksi not found",
+          success: false,
+        });
+      }
+
+    }
+    ).catch(() => {
+      res.status(500).json({
+        message: "Transaksi not found",
+        success: false,
+      });
+    })
+  },
 
 };
