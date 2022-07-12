0948ce316ad676cb1dbc68bd555731ded686bc26
jarot_stwn <jarotsetiawan1001@gmail.com>
2022-07-12 22:14:18 +0700

    change controller notif

--- controller/notifController.js
+++ controller/notifController.js
@@ -3,16 +3,36 @@ const {Op} = require("sequelize")
 
 module.exports = {
     getNotif: (req, res) => {
-        notifProduct.findAll({include: [
-            {
-                model: product,
-                as: "product",
-                where: {userId: req.user.id},
-            }
-        ]}).then((notif) =>{
-            res.json({message: "Get All notif", success: true, data: notif})
+        let listProduct = [];
+        product.findAll({where: {userId: req.user.id}})
+        .then(products => {
+            products.forEach((pdc) =>{
+                console.log(pdc.id)
+                listProduct.push(pdc.id)
+                console.log(listProduct)
+            })
+            notifProduct.findAll({
+                where: {[Op.or]: [{productId: {[Op.in]: listProduct}}, {userId: req.user.id}] },
+                include: [
+                {
+                    model: product,
+                    as: "product",
+                }
+            ]}).then((notif) =>{
+                res.json({message: "Get All notif", success: true, data: notif})
+            }).catch((err) =>{
+                res.json({message: "Cannot Get All notif", success: false, data: err.message})
+            })
+        })
+        console.log(listProduct)
+    },
+    deleteNotif: (req, res) => {
+        const {tawarId} = req.params
+        notifProduct.destroy({where: {id: tawarId}})
+        .then((notif) =>{
+            res.json({message: "Success Delete notif", success: true, data: notif})
         }).catch((err) =>{
-            res.json({message: "Cannot Get All notif", success: false, data: err.message})
+            res.json({message: "Cannot Delete notif", success: false, data: err.message})
         })
     }
 }
--- router/notif.js
+++ router/notif.js
@@ -4,5 +4,6 @@ const restrict = require('../middleware/restrict')
 const notif = require('../controller/notifController')
 
 router.get('/notif', restrict, notif.getNotif)
+router.get('/notif/:tawarId', restrict, notif.deleteNotif)
 
 module.exports = router
