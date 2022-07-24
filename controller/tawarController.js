b1fad3df738935969100aa5f75a1854741f52a0e
taqiudin.ahyari <taqiudin.ahyari@gmail.com>
2022-07-24 15:53:25 +0700

    fix get all tawar

--- controller/tawarController.js
+++ controller/tawarController.js
@@ -52,9 +52,9 @@ module.exports = {
     },
     getTawar: (req, res) => {
         models.tawar.findAll({
-            where: {
-                userId: req.user.id
-            },
+            // where: {
+            //     userId: req.user.id
+            // },
             include: [
                 {
                     model: models.product,
